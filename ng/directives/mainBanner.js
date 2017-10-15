c54c1650ba9d5a8c0f81c1cc231774de1243f844
changeun <changp4186@gmail.com>
2017-10-15 13:15:44 +0900

    main banner Carousel 추가

--- ng/directives/mainBanner.js
+++ ng/directives/mainBanner.js
@@ -21,49 +21,61 @@ angular.module('app')
          });
        }
    };
-}]);
-//
-//
-// angular.module('ui.bootstrap.demo').controller('CarouselDemoCtrl', function ($scope) {
-//   $scope.myInterval = 5000;
-//   $scope.noWrapSlides = false;
-//   $scope.active = 0;
-//   var slides = $scope.slides = [];
-//   var currIndex = 0;
-//
-//   for (var i = 0; i < 4; i++) {
-//     $scope.addSlide();
-//   }
-//
-//   // Randomize logic below
-//
-//   function assignNewIndexesToSlides(indexes) {
-//     for (var i = 0, l = slides.length; i < l; i++) {
-//       slides[i].id = indexes.pop();
-//     }
-//   }
-//
-//   function generateIndexesArray() {
-//     var indexes = [];
-//     for (var i = 0; i < currIndex; ++i) {
-//       indexes[i] = i;
-//     }
-//     return shuffle(indexes);
-//   }
-//
-//   // http://stackoverflow.com/questions/962802#962890
-//   function shuffle(array) {
-//     var tmp, current, top = array.length;
-//
-//     if (top) {
-//       while (--top) {
-//         current = Math.floor(Math.random() * (top + 1));
-//         tmp = array[current];
-//         array[current] = array[top];
-//         array[top] = tmp;
-//       }
-//     }
-//
-//     return array;
-//   }
-// });
+}])
+
+.controller('CarouselCtrl', function ($scope) {
+    $scope.myInterval = 2500;
+    $scope.noWrapSlides = false;
+    $scope.active = 0;
+    var slides = $scope.slides = [];
+    var currIndex = 0;
+
+    $scope.addSlide = function() {
+        var newWidth = 600 + slides.length + 1;
+        slides.push({
+            image: ['/images/items/banner/1.jpg','/images/items/banner/2.jpg', '/images/items/banner/3.jpg'][slides.length % 3],
+            id: currIndex++
+        });
+    };
+
+    $scope.randomize = function() {
+        var indexes = generateIndexesArray();
+        assignNewIndexesToSlides(indexes);
+    };
+
+    for (var i = 0; i < 3; i++) {
+        $scope.addSlide();
+    }
+
+    // Randomize logic below
+
+    function assignNewIndexesToSlides(indexes) {
+        for (var i = 0, l = slides.length; i < l; i++) {
+            slides[i].id = indexes.pop();
+        }
+    }
+
+    function generateIndexesArray() {
+        var indexes = [];
+        for (var i = 0; i < currIndex; ++i) {
+            indexes[i] = i;
+        }
+        return shuffle(indexes);
+    }
+
+    // http://stackoverflow.com/questions/962802#962890
+    function shuffle(array) {
+        var tmp, current, top = array.length;
+
+        if (top) {
+            while (--top) {
+                current = Math.floor(Math.random() * (top + 1));
+                tmp = array[current];
+                array[current] = array[top];
+                array[top] = tmp;
+            }
+        }
+
+        return array;
+    }
+});
