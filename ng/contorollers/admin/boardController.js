d112cd4085c42dc0df08a200cc8a9cf1c8339b2d
changeun <changp4186@gmail.com>
2017-10-17 00:33:52 +0900

    get('/api/board') APi 추가

--- ng/contorollers/admin/boardController.js
+++ ng/contorollers/admin/boardController.js
@@ -1,7 +1,9 @@
 angular.module('boardController', [])
-    .controller('createBoardController', function($http){
+    .controller('createBoardController', function ($http) {
         var createBoardCtrl = this;
-        createBoardCtrl.doWrite  = function (boardData) {
-            console.log(boardData);
+        createBoardCtrl.doWrite = function (boardData) {
+            $http.post('/api/board', boardData).then(function (result) {
+                console.log(result);
+            })
         }
-    });
+    })
